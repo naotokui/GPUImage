e38833ea77b8a09ec76cbee3e9bdb8ff8b9012c4
Nao Tokui <tokui@qosmo.jp>
2014-05-23 10:56:16 +0900

    Workaround: frameBufferReferenceCount

--- framework/Source/GPUImageFramebuffer.m
+++ framework/Source/GPUImageFramebuffer.m
@@ -263,7 +263,13 @@ void dataProviderUnlockCallback (void *info, const void *data, size_t size);
         return;
     }
 
-    NSAssert(framebufferReferenceCount > 0, @"Tried to overrelease a framebuffer, did you forget to call -useNextFrameForImageCapture before using -imageFromCurrentFramebuffer?");
+    if (framebufferReferenceCount <= 0){
+#ifdef DEBUG
+        NSLog(@"Tried to overrelease a framebuffer, did you forget to call -useNextFrameForImageCapture before using -imageFromCurrentFramebuffer?");
+#endif
+        return;
+    }
+//    NSAssert(framebufferReferenceCount > 0, @"Tried to overrelease a framebuffer, did you forget to call -useNextFrameForImageCapture before using -imageFromCurrentFramebuffer?");
     framebufferReferenceCount--;
     if (framebufferReferenceCount < 1)
     {
